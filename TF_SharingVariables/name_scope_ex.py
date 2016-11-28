947af954e87d6d86f71a6a9c163adc67d97d5963
Tomokazu Ishii <tomok.ishii@gmail.com>
2016-11-28 09:41:26 +0900

    update .gitignore

--- TF_SharingVariables/name_scope_ex.py
+++ TF_SharingVariables/name_scope_ex.py
@@ -10,6 +10,9 @@
 
 '''
 
+import numpy as np
+import tensorflow as tf
+
 # tf.name_scope
 with tf.name_scope("my_scope"):
     v1 = tf.get_variable("var1", [1], dtype=tf.float32)
@@ -24,7 +27,7 @@ print(a.name)   # my_scope/Add:0
 # tf.variable_scope
 with tf.variable_scope("my_scope"):
     v1 = tf.get_variable("var1", [1], dtype=tf.float32)
-    v2 = tf.Variable(1, name="var2", dtype=tf.float32)
+    v2 = tf.Variable(1, name="var3", dtype=tf.float32)
     a = tf.add(v1, v2)
 
 print(v1.name)  # my_scope/var1:0
