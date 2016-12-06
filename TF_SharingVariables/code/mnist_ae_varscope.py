09da03c2ba12f62fa4be04fa969716b887a54a54
Tomokazu Ishii <tomok.ishii@gmail.com>
2016-12-06 17:57:26 +0900

    update TF_SharingVariables, mnist_ae_varscope.py

--- TF_SharingVariables/code/mnist_ae_varscope.py
+++ TF_SharingVariables/code/mnist_ae_varscope.py
@@ -15,7 +15,7 @@ import tensorflow as tf
 
 # Import data
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("../MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("../data/", one_hot=True)
 
 # Encoder Layer   
 class Encoder(object):
