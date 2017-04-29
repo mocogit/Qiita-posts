9aacfeebdb054e17817e713adf0e85d5e8caa8ad
Tomokazu Ishii <tomok.ishii@gmail.com>
2017-04-30 07:00:52 +0900

    update mnist_prep_ssl.py README.md

--- MNIST_prep_util/README.md
+++ MNIST_prep_util/README.md
@@ -3,8 +3,12 @@
 This is MNIST data utility to split 4 blocks, which is used for semi-supervised learning.
 This code is so-called "add-on" of MNIST data-loader in TensorFlow package.
 
-1. **mnist_prep.ssl.py**
+1. **mnist_prep_ssl.py**
 2. **mnist_tfkeras_ssl.py** - demo code using mnist_prep_ssl.py
 
-## ToDo
-Currently, I've implemented only random-sampling for labelled data. I'm going to add "bin-sampling" for very small labelled data condition.
+mnist_prep_ssl.py support 2 mode.
+
+1. "bin-sampling" - you will get label-balanced data(X_lab).
+2. "random-sampling" - you will get data(X_lab) including some level of inbalance.
+
+For small number of labelled data, you should use "bin-sampling" mode.
--- MNIST_prep_util/mnist_prep_ssl.py
+++ MNIST_prep_util/mnist_prep_ssl.py
@@ -16,10 +16,10 @@ def params():
       set parameter to allocate data samples
     '''
     params = {}
-    params['n_train_lab'] = 200
-    params['n_val'] = 5000
-    params['n_train_unlab'] = 60000 - 200 - 5000
-    params['percent_limit'] = (0.7, 1.3)    # accept +/-30%
+    params['n_train_lab'] = 100
+    params['n_val'] = 10000
+    params['n_train_unlab'] = 60000 - 100 - 10000
+    params['mode'] = None
 
     return params
 
@@ -28,12 +28,11 @@ def load_data_ssl(params, dirn='../data'):
     '''
       load MNIST data and split into 4 blocks
     '''
-    MAX_TRIAL = 999     # retry-max to check label balance
-    percent_limit = params['percent_limit']
-
     n_train_lab = params['n_train_lab']
     n_train_unlab = params['n_train_unlab']
     n_val = params['n_val']
+    mode = params['mode']
+
     mnist = input_data.read_data_sets(dirn, validation_size=n_val, 
                                       one_hot=True)
     n_train_total = mnist.train.num_examples
@@ -42,27 +41,18 @@ def load_data_ssl(params, dirn='../data'):
 
     X_train = mnist.train.images
     y_train = mnist.train.labels
-    # prep. index list
-    n_train = X_train.shape[0]
 
-    num_trial = 0
-    while num_trial < MAX_TRIAL:
-        train_idx = np.random.permutation(n_train)
-        # split train data into 2 (labeled / unlabeld)
-        X_train_lab = X_train[train_idx[:n_train_lab]]
-        y_train_lab = y_train[train_idx[:n_train_lab]]
-        X_train_unlab = X_train[train_idx[n_train_lab:]]
-        y_train_unlab = y_train[train_idx[n_train_lab:]]
-        # check balance of label 
-        if check_label_balance(y_train_lab, percent_limit=percent_limit):
-            break
-        num_trial += 1
-        if num_trial == MAX_TRIAL:
-            raise ValueError('percentage range looks too narrow.')
+    # select bin_sampling or random_sampling (including inbalance)
+    if mode == 'random':
+        X_train_lab, X_train_unlab, y_train_lab, y_train_unlab = \
+            random_sampling(X_train, y_train, n_labeled=n_train_lab)
+    else:
+        X_train_lab, X_train_unlab, y_train_lab, y_train_unlab = \
+            bin_sampling(X_train, y_train, n_labeled=n_train_lab)
 
-    # cancel scaling of DataSet class constructor
-    X_train_lab = X_train[train_idx[:n_train_lab]] * 255.
-    X_train_unlab = X_train[train_idx[n_train_lab:]] * 255.
+    # cancel scaling by DataSet class constructor
+    X_train_lab = X_train_lab * 255.
+    X_train_unlab = X_train_unlab * 255.
 
     train_lab = DataSet(X_train_lab, y_train_lab, reshape=False)
     train_unlab = DataSet(X_train_unlab, y_train_unlab, reshape=False)
@@ -74,13 +64,48 @@ def load_data_ssl(params, dirn='../data'):
     return mnist_ssl
 
 
-def check_label_balance(y_label_np, percent_limit=(0.85, 1.15)):
+def bin_sampling(X_train, y_train, n_labeled=100):
+    '''
+      sampling to make each label same number
+      args:
+        X_train, y_train    : source dataset
+        n_labeled           : total labeled data
+    '''
+    num_examples = X_train.shape[0]
+    lab_each = n_labeled // 10
+    cnt = np.zeros([10,], dtype=np.int)
+    index_picked = [[] for lab in range(10)]
+
+    for index, y in enumerate(y_train):
+        y_i = np.argmax(y)
+        if cnt[y_i] < lab_each:
+            cnt[y_i] += 1
+            index_picked[y_i].append(index)
+
+        if np.min(cnt) == lab_each:
+            break
+
+    index_picked = np.asarray(index_picked)
+    index_picked = index_picked.ravel()
+    
+    index_not_picked = set(np.arange(num_examples)) - set(index_picked)
+    index_not_picked = np.asarray(list(index_not_picked))
+
+    X_lab = X_train[index_picked]
+    y_lab = y_train[index_picked]
+    X_unlab = X_train[index_not_picked]
+    y_unlab = y_train[index_not_picked]
+
+    return X_lab, X_unlab, y_lab, y_unlab
+
+
+def check_label_balance(y_label_np, percent_limit=(0.8, 1.2)):
     '''
       check label balance of sampled data
       args:
         y_label_np    - label array (one-hot encoded)
         percent_limit - limit percent of each label sample
-                        if it set (0.85, 1.15), acceptable percent is +/-15%
+                        if set (0.85, 1.15), acceptable percent is +/-15%
     '''
     n_sample = y_label_np.shape[0]
     counts = np.zeros([10], dtype=np.int32)
@@ -101,15 +126,41 @@ def check_label_balance(y_label_np, percent_limit=(0.85, 1.15)):
     return judge
 
 
+def random_sampling(X_train, y_train, n_labeled=100):
+    '''
+      sampling data by random
+    '''
+    MAX_TRIAL = 999     # retry-max to check label balance
+    n_train = X_train.shape[0]
+
+    num_trial = 0
+    while num_trial < MAX_TRIAL:
+        # use numpy.random.permutation
+        train_idx = np.random.permutation(n_train)
+        # split train data into 2 (labeled / unlabeld)
+        X_lab = X_train[train_idx[:n_labeled]]
+        y_lab = y_train[train_idx[:n_labeled]]
+        X_unlab = X_train[train_idx[n_labeled:]]
+        y_unlab = y_train[train_idx[n_labeled:]]
+        # check balance of label 
+        if check_label_balance(y_lab):
+            break
+        num_trial += 1
+        if num_trial == MAX_TRIAL:
+            raise ValueError('percentage range looks too narrow.')    
+
+    return X_lab, X_unlab, y_lab, y_unlab
+
+
 def test_load_data_ssl(dirn):
     data_alloc = params()
     mnist = load_data_ssl(data_alloc, dirn=dirn)
     # read data
     print('\nSome test results:')
     print('num samples of train data without label = ',
-        mnist.train_lab.num_examples)
-    print('num samples of train data w/ label = ',
         mnist.train_unlab.num_examples)
+    print('num samples of train data w/ label = ',
+        mnist.train_lab.num_examples)
     print('num samples of validation data = ',
         mnist.validation.num_examples)
     print('num samples of test data = ',
@@ -125,4 +176,6 @@ def test_load_data_ssl(dirn):
 
 
 if __name__ == '__main__':
-    test_load_data_ssl(dirn='../data')
+    # test_load_data_ssl(dirn='../data')
+    dirn = '/home/tomokazu/Sources/Data/MNIST'
+    test_load_data_ssl(dirn=dirn)
